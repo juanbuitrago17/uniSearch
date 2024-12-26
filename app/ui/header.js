7bed9407a38232e7bb36487e2e89834c5321697c
juan pablo buitrago <90221646+juanbuitrago17@users.noreply.github.com>
2024-12-26 03:44:26 +0000

    feat: add and update menu for mobile

--- app/ui/header.js
+++ app/ui/header.js
@@ -1,18 +1,38 @@
+"use client";
+
+import React, { useState } from 'react';
+import './header.css';
 
 export default function Header() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
     return (
         <header className="bg-gray-800 border-b border-gray-700">
-            <div className="container mx-auto flex items-center justify-between py-4 px-6">
+            <div className="mx-auto py-4 px-6 casa" >
                 <h1 className="text-3xl font-bold text-white">Unisearch</h1>
-                <nav>
-                    <ul className="flex space-x-6">
-                        <li><a href="#" className="text-gray-300 hover:text-white">Home</a></li>
-                        <li><a href="#about" className="text-gray-300 hover:text-white">About</a></li>
-                        <li><a href="#features" className="text-gray-300 hover:text-white">Features</a></li>
-                        <li><a href="#contact" className="text-gray-300 hover:text-white">Contact</a></li>
-                    </ul>
-                </nav>
+                <div className="menu-container">
+                    {/* Mostrar solo el ícono en pantallas pequeñas */}
+                    <label 
+                        htmlFor="menu-toggle" 
+                        className={`menu-icon`}
+                        onClick={toggleMenu}
+                    >
+                        {menuOpen ? '✖' : '☰'}
+                    </label>
+
+                    {/* Menú en la barra de título (solo en pantallas grandes) */}
+                    <nav className={`menu ${menuOpen ? 'open' : ''}`}>
+                        <a href="#" className="menu-item">Home</a>
+                        <a href="#" className="menu-item">About</a>
+                        <a href="#" className="menu-item">Features</a>
+                        <a href="#" className="menu-item">Contact</a>
+                    </nav>
+                </div>
             </div>
         </header>
-    )
+    );
 }
