72801819fa922271de9380ff77ee5da2bcf6d7dc
juan pablo buitrago <90221646+juanbuitrago17@users.noreply.github.com>
2024-12-27 04:13:18 +0000

    feat: add google analytic

--- app/layout.js
+++ app/layout.js
@@ -20,13 +20,30 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* Script de Google Analytics */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-V8MZEDHPHH"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-V8MZEDHPHH');
+            `,
+          }}
+        ></script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <div className="bg-gray-900 text-gray-100 min-h-screen">
-        <Header />
+          <Header />
 
-        {children}
+          {children}
         </div>
       </body>
     </html>
