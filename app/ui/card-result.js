65c7ee6c811f25a70b32c6f0765c08d35173a8ba
juan pablo buitrago <90221646+juanbuitrago17@users.noreply.github.com>
2024-12-30 23:54:39 +0000

    feat: add pagination and mistery planet

--- app/ui/card-result.js
+++ app/ui/card-result.js
@@ -2,11 +2,12 @@ import './card-result.css';
 import Link from 'next/link';
 
 export default function CardResult({ result }) {
+    const imagen = result['image_url'] || '/images/misteryplanet.png';
     return (
         <Link href={`/information/${result._id}`}  >
             <div className="card">
             <div className='description_imagen'>
-            <img src={result['image_url']} width="300px" alt={result.name}/>
+            <img src={imagen} width="300px" alt={result.name}/>
             </div>
             <div className='description'>
             <h3 className="cardTitle">{result.name}</h3>
--- app/ui/results.js
+++ app/ui/results.js
@@ -1,17 +1,62 @@
 import CardResult from "./card-result";
-
+import { useState } from "react";
 import './results.css';
 
-export default function Results({data}) {
+export default function Results({ data }) {
+    const [currentPage, setCurrentPage] = useState(1);
+    const itemsPerPage = 12;
+
+    // Calculate total pages
+    const totalPages = Math.ceil(data.length / itemsPerPage);
+
+    // Get current page data
+    const currentResults = data.slice(
+        (currentPage - 1) * itemsPerPage,
+        currentPage * itemsPerPage
+    );
+
+    
+
+    const goToPage = (pageNumber) => {
+        if (pageNumber >= 1 && pageNumber <= totalPages) {
+          setCurrentPage(pageNumber);
+        }
+      };
+      
     return (
         <div className="results">
             <h2>Results</h2>
             <div className="results__items">
-                {
-                    data.map(result => (
-                        <CardResult key={result['_id']} result={result}></CardResult>
-                    ))
-                }
+                {currentResults.map(result => (
+                    <CardResult key={result['_id']} result={result} />
+                ))}
+            </div>
+
+            {/* Pagination Controls */}
+            <div className="pagination-controls">
+                {/* Previous Button */}
+                <button className="button-pagination" onClick={() => goToPage(currentPage - 1)} disabled={currentPage === 1}>
+                    Previous
+                </button>
+
+                {/* Dynamic Page Numbers */}
+                {[...Array(totalPages)].map((_, index) => {
+                    const pageNumber = index + 1;
+                    return (
+                        <button
+                            key={pageNumber}
+                            onClick={() => goToPage(pageNumber)}
+                            className={`button-number ${currentPage === pageNumber ? 'active' : ''}`}
+                        >
+                            {pageNumber}
+                        </button>
+                    );
+                })}
+
+                {/* Next Button */}
+                <button className="button-pagination" onClick={() => goToPage(currentPage + 1)} disabled={currentPage === totalPages}>
+                    Next
+                </button>
             </div>
         </div>
     )
